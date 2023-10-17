5f8d1d5a7692f08329965ac057b407738d741f37
Anderson <Anderson.iuse@gmail.com>
2023-10-17 19:25:41 -0300

    chore: add next auth setup

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,13 +1,52 @@
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import { MenuIcon, ShoppingCartIcon, LogInIcon, PercentIcon, ListOrderedIcon, HomeIcon } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
+import {
+    Sheet,
+    SheetContent,
+    SheetFooter,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+  
 
 const Header = () => {
     return ( 
        <Card className="flex p-[1.8rem] justify-between items-center">
-        <Button size="icon" variant="outline">
-            <MenuIcon />
-        </Button>
+        <Sheet>
+            <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+                <MenuIcon />
+            </Button>
+            </SheetTrigger>
+            <SheetContent side="left">
+                <SheetFooter className="text-left text-lg font-semibold">
+                    Menu
+                </SheetFooter>
+
+                <div className="mt-2 flex flex-col gap-2">
+                    <Button className="w-full justify-start gap-5" variant="outline">
+                        <LogInIcon />
+                        Fazer login
+                    </Button>
+
+                    <Button className="w-full justify-start gap-5" variant="outline">
+                        <HomeIcon />
+                       Início
+                    </Button>
+                   
+                    <Button className="w-full justify-start gap-5" variant="outline">
+                        <PercentIcon />
+                       Ofertas
+                    </Button>
+
+                    <Button className="w-full justify-start gap-5" variant="outline">
+                        <ListOrderedIcon />
+                       Catálogo
+                    </Button>
+                </div>
+            </SheetContent>
+        </Sheet>
+
         
         <h1 className="font-semibold text-lg"><span className="text-primary">FWS</span> Store</h1>
 
